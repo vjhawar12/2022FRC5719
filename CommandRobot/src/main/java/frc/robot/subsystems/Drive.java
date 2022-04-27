309d5c7850f4bd7e80401a314df33c19bea3a105
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2022-04-27 15:12:03 -0400

    Update Drive.java

--- CommandRobot/src/main/java/frc/robot/subsystems/Drive.java
+++ CommandRobot/src/main/java/frc/robot/subsystems/Drive.java
@@ -8,24 +8,27 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup; 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX; 
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX; 
 
 public class Drive extends SubsystemBase {
-    protected TalonSRX motorFrontLeft = new TalonSRX(Constants.talonFrontLeft); 
-    protected TalonSRX motorFrontRight = new TalonSRX(Constants.talonFrontRight); 
-    protected TalonSRX motorBackLeft = new TalonSRX(Constants.talonBackLeft); 
-    protected TalonSRX motorBackRight = new TalonSRX(Constants.talonBackRight); 
 
-    protected PWM motor = new PWM(Constants.pwm); 
+    PWM motor = new PWM(Constants.pwm); 
 
-    MotorController leftMotorController; 
-    MotorController rightMotorController; 
-
-    MotorControllerGroup left = new MotorControllerGroup(leftMotorController); 
-    MotorControllerGroup right = new MotorControllerGroup(rightMotorController); 
+    MotorController m_frontLeft = new PWMVictorSPX(1);
+    MotorController m_rearLeft = new PWMVictorSPX(2);
+    MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
  
-    DifferentialDrive m_drive = new DifferentialDrive(left, right);
+    MotorController m_frontRight = new PWMVictorSPX(3);
+    MotorController m_rearRight = new PWMVictorSPX(4);
+    MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
+ 
+    DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
     
     public void driveSystem() {
-
+        
     } 
+
+    public void useJoystick(Joystick joystick) {
+        
+    }
 }
