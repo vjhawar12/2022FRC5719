99b6f5f0e7d3f843797227387074af62ccb339ae
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2022-04-27 15:47:25 -0400

    Update DriveSubsystem.java

--- CommandRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ CommandRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,16 +8,9 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup; 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX; 
 import frc.robot.Constants;
-<<<<<<< HEAD
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX; 
-
-public class Drive extends SubsystemBase {
-=======
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX; 
 
 public class DriveSubsystem extends SubsystemBase {
->>>>>>> 45798a64f8a06e8daa4ee93ff5f5ad5715333633
 
     PWM motor = new PWM(Constants.pwm); 
 
@@ -29,39 +22,6 @@ public class DriveSubsystem extends SubsystemBase {
     MotorController m_rearRight = new PWMVictorSPX(4);
     MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
  
-<<<<<<< HEAD
-    DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
-    
-    public void driveSystem() {
-        
-    } 
-
-    public void useJoystick(Joystick joystick) {
-        
-    }
-=======
-
-public class Drive extends SubsystemBase {
-    protected TalonSRX motorFrontLeft = new TalonSRX(Constants.talonFrontLeft); 
-    protected TalonSRX motorFrontRight = new TalonSRX(Constants.talonFrontRight); 
-    protected TalonSRX motorBackLeft = new TalonSRX(Constants.talonBackLeft); 
-    protected TalonSRX motorBackRight = new TalonSRX(Constants.talonBackRight); 
-
-    protected PWM motor = new PWM(Constants.pwm); 
-
-    MotorController leftMotorController; 
-    MotorController rightMotorController; 
-
-    MotorControllerGroup left = new MotorControllerGroup(leftMotorController); 
-    MotorControllerGroup right = new MotorControllerGroup(rightMotorController); 
- 
-    DifferentialDrive m_drive = new DifferentialDrive(left, right);
-    
-    public void driveSystem() {
-
-    } 
->>>>>>> 3d33c6485e555eeca98d2b8235b362ca2fa94afb
-=======
     DifferentialDrive m_drive; 
 
     DriveSubsystem() {
@@ -84,5 +44,4 @@ public class Drive extends SubsystemBase {
         m_drive.stopMotor(); 
     }
 
->>>>>>> 45798a64f8a06e8daa4ee93ff5f5ad5715333633
 }
