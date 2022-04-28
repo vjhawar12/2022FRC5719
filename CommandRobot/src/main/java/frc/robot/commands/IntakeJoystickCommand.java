5b0ba731a4b15fb3e1f47c97cd1e6336686439b4
vjhawar12 <vedantj1203@gmail.com>
2022-04-28 16:47:27 -0400

    added sensor

--- CommandRobot/src/main/java/frc/robot/commands/IntakeJoystickCommand.java
+++ CommandRobot/src/main/java/frc/robot/commands/IntakeJoystickCommand.java
@@ -1,5 +1,16 @@
 package frc.robot.commands;
 
-public class IntakeJoystickCommand {
-    
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class IntakeJoystickCommand extends SequentialCommandGroup {
+    IntakeJoystickCommand() {
+
+    }
+
+    @Override
+    protected void initialize() {
+        RobotContainer.intakeSubsystem.stop(); 
+    }
 }
