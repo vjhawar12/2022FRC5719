0769fc2aaa5df014b1d0ab5d23b0981f10dcb4f3
vjhawar12 <vedantj1203@gmail.com>
2022-04-28 11:59:58 -0400

    Fixed robot container class

--- CommandRobot/src/main/java/frc/robot/commands/DriveCommand.java
+++ CommandRobot/src/main/java/frc/robot/commands/DriveCommand.java
@@ -2,19 +2,14 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot; 
 
 public class DriveCommand extends CommandBase {
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DriveSubsystem m_subsystem;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
   public DriveCommand(DriveSubsystem subsystem) {
     m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
@@ -28,7 +23,9 @@ public class DriveCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+      
+  }
 
   // Returns true when the command should end.
   @Override
