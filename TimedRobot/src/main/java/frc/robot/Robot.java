b7878360c0000148f448989e159a68d6deabcabc
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2022-06-10 17:23:36 -0400

    Update Robot.java

--- TimedRobot/src/main/java/frc/robot/Robot.java
+++ TimedRobot/src/main/java/frc/robot/Robot.java
@@ -3,48 +3,35 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode; 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot implements Constants {
   protected Timer timer; 
   protected double forwardTorque, lateralTorque; 
 
-  protected final VictorSPX leftFront  = new VictorSPX(LEFT_FRONT); 
-  protected final VictorSPX rightFront = new VictorSPX(RIGHT_FRONT); 
-  protected final VictorSPX rightFollower = new VictorSPX(RIGHT_FOLLOWER); 
-  protected final VictorSPX leftFollower = new VictorSPX(LEFT_FOLLOWER); 
+  protected final WPI_VictorSPX leftFront  = new WPI_VictorSPX(LEFT_FRONT); 
+  protected final WPI_VictorSPX rightFront = new WPI_VictorSPX(RIGHT_FRONT);   
+  protected final WPI_VictorSPX rightFollower = new WPI_VictorSPX(RIGHT_FOLLOWER); 
+  protected final WPI_VictorSPX leftFollower = new WPI_VictorSPX(LEFT_FOLLOWER); 
 
-  public PS4Controller controller = new PS4Controller(0);  
+  DifferentialDrive drive = new DifferentialDrive(leftFront, rightFront); 
 
-  private void driveAuto(double vel) {  
-    rightFront.set(ControlMode.PercentOutput, vel);
-    leftFront.set(ControlMode.PercentOutput, vel); 
-    rightFollower.set(ControlMode.Follower, RIGHT_FRONT); 
-    leftFollower.set(ControlMode.Follower, LEFT_FRONT); 
-}
-
-  private void driveTeleop(double forward, double lateral) {
-    rightFront.set(ControlMode.PercentOutput, forward);
-    leftFront.set(ControlMode.PercentOutput, lateral); 
-    rightFollower.set(ControlMode.Follower, RIGHT_FRONT); 
-    leftFollower.set(ControlMode.Follower, LEFT_FRONT);
-  }
+  public PS4Controller controller = new PS4Controller(JOYSTICK_PORT);  
 
   @Override
   public void autonomousInit() {
     timer = new Timer(); 
     timer.reset(); 
     timer.start();
-    timer.stop();
-  
   }
 
   @Override
   public void autonomousPeriodic() {
     if (timer.get() < 2.0) {
-      driveAuto(100); 
+      drive.arcadeDrive(100, 30);
     }
   }
 
@@ -57,8 +44,10 @@ public class Robot extends TimedRobot implements Constants {
   @Override
   public void teleopPeriodic() {
     forwardTorque = controller.getRawAxis(JOYSTICK_LEFT); 
-    lateralTorque = -1 * controller.getRawAxis(JOYSTICK_RIGHT); 
-    driveTeleop(forwardTorque, lateralTorque); 
+    lateralTorque = controller.getRawAxis(JOYSTICK_RIGHT); 
+    leftFollower.follow(leftFront); 
+    rightFollower.follow(rightFront); 
+    drive.arcadeDrive(lateralTorque, forwardTorque);
   }
 } 
 
